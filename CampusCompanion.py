a1b2406e4a6d7926f0e48f525cda2f0956e4c94f
Imran Bepari <imranbepari97@gmail.com>
2018-03-10 13:33:18 +0000

    Added gunicorn, HTML template with Bootstrap

--- CampusCompanion.py
+++ CampusCompanion.py
@@ -1,3 +1,4 @@
+import flask
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return flask.render_template('index.html')
 
 
 if __name__ == '__main__':
