dfb9e180a3a3a0111c4997613a79601eda2367d5
patrascu e.r. (erp3g15) <erp3g15@soton.ac.uk>
2018-03-10 15:59:55 +0000

    created database models; all db stuff should work

--- CampusCompanion.py
+++ CampusCompanion.py
@@ -1,7 +1,22 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+POSTGRES = {
+    'user': 'campus',
+    'pw': 'companion',
+    'db': 'CampusCompanion',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://%(user)s:%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
+db = SQLAlchemy(app)
+
+from models import Issue, User
 
 @app.route('/')
 def hello_world():
@@ -9,4 +24,5 @@ def hello_world():
 
 
 if __name__ == '__main__':
+    db.create_all()
     app.run()
